485974f9bd4da12c21606d80732cee0aea7639b2
Nguyen Nhat Truong <69668630+chumxo33@users.noreply.github.com>
2020-12-31 19:44:06 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,121 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link'
+
+const defaultEndpoint ='https://rickandmortyapi.com/api/character/';
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint)
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
+export default function Home({ data }) {
+  const { info, results: defaultResults = [] } = data;
+  const [results, updateResults] = useState(defaultResults);
+  const [page, updatePage] = useState({
+    ...info,
+    current: defaultEndpoint
+  });
+  const { current } = page;
+
+    useEffect(() => {
+    if ( current === defaultEndpoint ) return;
+  
+    async function request() {
+      const res = await fetch(current)
+      const nextData = await res.json();
+  
+      updatePage({
+        current,
+        ...nextData.info
+      });
+  
+      if ( !nextData.info?.prev ) {
+        updateResults(nextData.results);
+        return;
+      }
+  
+      updateResults(prev => {
+        return [
+          ...prev,
+          ...nextData.results
+        ]
+      });
+    }
+  
+    request();
+  }, [current]);
+  function handleLoadMore() {
+    updatePage(prev => {
+      return {
+        ...prev,
+        current: page?.next
+      }
+    });
+  }
+
+  function handleOnSubmitSearch(e) {
+    e.preventDefault();
+  
+    const { currentTarget = {} } = e;
+    const fields = Array.from(currentTarget?.elements);
+    const fieldQuery = fields.find(field => field.name === 'query');
+  
+    const value = fieldQuery.value || '';
+    const endpoint = `https://rickandmortyapi.com/api/character/?name=${value}`;
+  
+    updatePage({
+      current: endpoint
+    });
+  }
+  
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Wubba Lubba Dub Dub!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Wubba Lubba Dub Dub!
+        </h1>
+
+        <p className={styles.description}>
+          Rick và Morty Character Wiki
+        </p>
+
+        <form className={styles.search} onSubmit={handleOnSubmitSearch}>
+            <input name="query" type="search" />
+            <button>Search</button>
+        </form>
+
+        <ul className={styles.grid}>
+          {results.map(result => {
+             const { id, name,image } = result; 
+             return (
+              <li key={id} className="card">
+                  <Link href="/character/[id]" as={`/character/${id}`}>
+              <a>
+              <img src={image} />
+                <h3>{ name }</h3>
+              </a>
+              </Link>
+              </li>
+              )
+
+          })}
+        </ul>
+
+      </main>
+
+      
+    </div>
+  )
+}
+
